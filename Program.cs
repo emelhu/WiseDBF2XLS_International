0c2b81b095a16453df68c5a54fed7abc5541ce2f
Laszlo Moravecz <emel@emel.hu>
2016-04-15 21:48:53 +0200

    Start

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiseDBF2XLS_International
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
